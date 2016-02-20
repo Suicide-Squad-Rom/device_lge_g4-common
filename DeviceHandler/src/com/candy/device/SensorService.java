CandyDevices/device_htc_m8

add screen off gestures

DeviceHandler/src/com/candy/device/SensorService.java
@@ -1,0 +1,160 @@
+/*
+ * Copyright (C) 2014 candyRoms Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.candy.device;
+
+import android.app.Activity;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.IBinder;
+import android.os.PowerManager;
+import android.os.SystemClock;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.Iterator;
+
+import com.android.internal.util.candy.ActionConstants;
+import com.android.internal.util.candy.Action;
+
+import com.candy.device.settings.ScreenOffGesture;
+
+public class SensorService extends Service implements SensorEventListener {
+
+    private static final boolean DEBUG = false;
+
+    public static final String TAG = "SensorService";
+
+    public static final String KEY_ENABLE_GESTURES = "enable_gestures";
+
+    public static final String HTC_GESTURES = "hTC Gesture Motion HIDI";
+
+    // Gestures
+    private static final int DOUBLE_TAP = 15;
+    private static final int SWIPE_UP = 2;
+    private static final int SWIPE_DOWN = 3;
+    private static final int SWIPE_RIGHT = 4;
+    private static final int SWIPE_LEFT = 5;
+    private static final int CAMERA = 6;
+
+
+    private Context mContext;
+    private SharedPreferences mPrefs;
+    private ScreenStateReceiver mScreenStateReceiver;
+    private SensorManager mSensorManager;
+    private Sensor mSensor = null;
+    private SensorEventListener mSensorEventListener;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mContext = getApplicationContext();
+        mPrefs = getSharedPreferences(ScreenOffGesture.GESTURE_SETTINGS, Activity.MODE_PRIVATE);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        mSensorEventListener = this;
+
+        Iterator iterator = mSensorManager.getSensorList(-1).iterator();
+        while (iterator.hasNext()) {
+            Sensor sensor = (Sensor) iterator.next();
+            if (sensor.getName().equals(HTC_GESTURES)) {
+                if (DEBUG) Log.d(TAG, "found gesture sensor");
+                mSensor = sensor;
+            }
+        }
+        if (mSensor != null) {
+            mSensorManager.registerListener(mSensorEventListener,
+                    mSensor, SensorManager.SENSOR_DELAY_FASTEST);
+            mScreenStateReceiver = new ScreenStateReceiver();
+            IntentFilter intentFilter = new IntentFilter();
+            intentFilter.addAction("android.intent.action.SCREEN_ON");
+            intentFilter.addAction("android.intent.action.SCREEN_OFF");
+            registerReceiver(mScreenStateReceiver, intentFilter);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mSensorManager.unregisterListener(this);
+        unregisterReceiver(mScreenStateReceiver);
+    }
+
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    public final void onAccuracyChanged(Sensor sensor, int i) {
+    }
+
+    public final void onSensorChanged(SensorEvent sensorEvent) {
+        mSensorManager.unregisterListener(mSensorEventListener);
+        float gesture = sensorEvent.values[0];
+        if (DEBUG) Log.d(TAG, "Sensor type=" + sensorEvent.sensor.getType()
+                + "," + sensorEvent.values[0] + "," + sensorEvent.values[1]);
+        startAction((int) gesture);
+    }
+
+    private void startAction(int gesture) {
+        String action = null;
+        switch (gesture) {
+            case DOUBLE_TAP:
+                action = mPrefs.getString(ScreenOffGesture.PREF_DOUBLE_TAP,
+                        ActionConstants.ACTION_WAKE_DEVICE);
+                break;
+            case SWIPE_UP:
+                action = mPrefs.getString(ScreenOffGesture.PREF_SWIPE_UP,
+                        ActionConstants.ACTION_TORCH);
+                break;
+            case SWIPE_DOWN:
+                action = mPrefs.getString(ScreenOffGesture.PREF_SWIPE_DOWN,
+                        ActionConstants.ACTION_MEDIA_PLAY_PAUSE);
+                break;
+            case SWIPE_LEFT:
+                action = mPrefs.getString(ScreenOffGesture.PREF_SWIPE_LEFT,
+                        ActionConstants.ACTION_MEDIA_PREVIOUS);
+                break;
+            case SWIPE_RIGHT:
+                action = mPrefs.getString(ScreenOffGesture.PREF_SWIPE_RIGHT,
+                        ActionConstants.ACTION_MEDIA_NEXT);
+                break;
+            case CAMERA:
+                action = mPrefs.getString(ScreenOffGesture.PREF_CAMERA,
+                        ActionConstants.ACTION_CAMERA);
+                break;
+        }
+        if (action == null || action != null && action.equals(ActionConstants.ACTION_NULL)) {
+            return;
+        }
+        if (action.equals(ActionConstants.ACTION_CAMERA)
+                || !action.startsWith("**")) {
+            Action.processAction(mContext, ActionConstants.ACTION_WAKE_DEVICE, false);
+        }
+        if (DEBUG) Log.d(TAG, action + ",gesture=" + gesture);
+        mSensorManager.registerListener(mSensorEventListener,
+                mSensor, SensorManager.SENSOR_DELAY_FASTEST);
+        Action.processAction(mContext, action, false);
+    }
+}

DeviceHandler/src/com/candy/device/util/ShortcutPickerHelper.java
@@ -1,0 +1,176 @@
+/*
+ * Copyright (C) 2011 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.candy.device.util;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
+import android.content.Intent.ShortcutIconResource;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.Log;
+
+import com.candy.device.R;
+
+import com.android.internal.util.candy.AppHelper;
+
+import java.util.ArrayList;
+
+public class ShortcutPickerHelper {
+
+    public static final int REQUEST_PICK_SHORTCUT = 100;
+    public static final int REQUEST_PICK_APPLICATION = 101;
+    public static final int REQUEST_CREATE_SHORTCUT = 102;
+
+    private Activity mParent;
+    private OnPickListener mListener;
+    private PackageManager mPackageManager;
+    private int lastFragmentId;
+
+    public interface OnPickListener {
+        void shortcutPicked(String uri, String friendlyName, Bitmap bmp, boolean isApplication);
+    }
+
+    public ShortcutPickerHelper(Activity parent, OnPickListener listener) {
+        mParent = parent;
+        mPackageManager = mParent.getPackageManager();
+        mListener = listener;
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            switch (requestCode) {
+                case REQUEST_PICK_APPLICATION:
+                    completeSetCustomApp(data);
+                    break;
+                case REQUEST_CREATE_SHORTCUT:
+                    completeSetCustomShortcut(data);
+                    break;
+                case REQUEST_PICK_SHORTCUT:
+                    processShortcut(data, REQUEST_PICK_APPLICATION, REQUEST_CREATE_SHORTCUT);
+                    break;
+            }
+        }
+    }
+
+    public void pickShortcut(int fragmentId) {
+        pickShortcut(fragmentId, false);
+    }
+
+    public void pickShortcut(int fragmentId, boolean fullAppsOnly) {
+        lastFragmentId = fragmentId;
+
+        if (fullAppsOnly) {
+            Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+            mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+            Intent pickIntent = new Intent(Intent.ACTION_PICK_ACTIVITY);
+            pickIntent.putExtra(Intent.EXTRA_INTENT, mainIntent);
+            startFragmentOrActivity(pickIntent, REQUEST_PICK_APPLICATION);
+        } else {
+            Bundle bundle = new Bundle();
+
+            ArrayList<String> shortcutNames = new ArrayList<String>();
+            shortcutNames.add(mParent.getString(R.string.group_applications));
+            bundle.putStringArrayList(Intent.EXTRA_SHORTCUT_NAME, shortcutNames);
+
+            ArrayList<ShortcutIconResource> shortcutIcons = new ArrayList<ShortcutIconResource>();
+            shortcutIcons.add(ShortcutIconResource.fromContext(mParent,
+                    android.R.drawable.sym_def_app_icon));
+            bundle.putParcelableArrayList(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, shortcutIcons);
+
+            Intent pickIntent = new Intent(Intent.ACTION_PICK_ACTIVITY);
+            pickIntent.putExtra(Intent.EXTRA_INTENT, new Intent(Intent.ACTION_CREATE_SHORTCUT));
+            pickIntent.putExtra(Intent.EXTRA_TITLE, mParent.getText(
+                    R.string.select_custom_app_title));
+            pickIntent.putExtras(bundle);
+            startFragmentOrActivity(pickIntent, REQUEST_PICK_SHORTCUT);
+        }
+    }
+
+    private void startFragmentOrActivity(Intent pickIntent, int requestCode) {
+        if (lastFragmentId == 0) {
+            mParent.startActivityForResult(pickIntent, requestCode);
+        } else {
+            Fragment cFrag = mParent.getFragmentManager().findFragmentById(lastFragmentId);
+            if (cFrag != null) {
+                mParent.startActivityFromFragment(cFrag, pickIntent, requestCode);
+            }
+        }
+    }
+
+    private void processShortcut(Intent intent,
+        int requestCodeApplication, int requestCodeShortcut) {
+        // Handle case where user selected "Applications"
+        String applicationName = mParent.getResources().getString(R.string.group_applications);
+        String shortcutName = intent.getStringExtra(Intent.EXTRA_SHORTCUT_NAME);
+
+        if (applicationName != null && applicationName.equals(shortcutName)) {
+            Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+            mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+            Intent pickIntent = new Intent(Intent.ACTION_PICK_ACTIVITY);
+            pickIntent.putExtra(Intent.EXTRA_INTENT, mainIntent);
+            startFragmentOrActivity(pickIntent, requestCodeApplication);
+        } else {
+            startFragmentOrActivity(intent, requestCodeShortcut);
+        }
+    }
+
+    private void completeSetCustomApp(Intent data) {
+        mListener.shortcutPicked(data.toUri(0),
+            AppHelper.getFriendlyActivityName(mParent, mPackageManager, data, false), null, true);
+    }
+
+    private void completeSetCustomShortcut(Intent data) {
+        Intent intent = data.getParcelableExtra(Intent.EXTRA_SHORTCUT_INTENT);
+        /* preserve shortcut name, we want to restore it later */
+        intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, data.getStringExtra(
+                Intent.EXTRA_SHORTCUT_NAME));
+        String appUri = intent.toUri(0);
+        appUri = appUri.replaceAll("com.android.contacts.action.QUICK_CONTACT", Intent.ACTION_VIEW);
+
+        // Check if icon is present
+        Bitmap bmp = null;
+        Parcelable extra = data.getParcelableExtra(Intent.EXTRA_SHORTCUT_ICON);
+        if (extra != null && extra instanceof Bitmap) {
+            bmp = (Bitmap) extra;
+        }
+        // No icon till now check if icon resource is present
+        if (bmp == null) {
+            extra = data.getParcelableExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE);
+            if (extra != null && extra instanceof Intent.ShortcutIconResource) {
+                try {
+                    Intent.ShortcutIconResource iconResource = (ShortcutIconResource) extra;
+                    Resources resources =
+                            mPackageManager.getResourcesForApplication(iconResource.packageName);
+                    final int id = resources.getIdentifier(iconResource.resourceName, null, null);
+                    bmp = BitmapFactory.decodeResource(resources, id);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        mListener.shortcutPicked(appUri,
+                AppHelper.getFriendlyShortcutName(mParent, mPackageManager, intent), bmp, false);
+    }
+
+}

Suicide-Squad-Rom/device_lge_g4-common

DeviceHandler/src/com/candy/device/SensorService.java
@@ -1,0 +1,160 @@
+/*
+ * Copyright (C) 2014 candyRoms Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.candy.device;
+
+import android.app.Activity;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.IBinder;
+import android.os.PowerManager;
+import android.os.SystemClock;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.Iterator;
+
+import com.android.internal.util.slim.ActionConstants;
+import com.android.internal.util.slim.Action;
+
+import com.candy.device.settings.ScreenOffGesture;
+
+public class SensorService extends Service implements SensorEventListener {
+
+    private static final boolean DEBUG = false;
+
+    public static final String TAG = "SensorService";
+
+    public static final String KEY_ENABLE_GESTURES = "enable_gestures";
+
+    public static final String HTC_GESTURES = "hTC Gesture Motion HIDI";
+
+    // Gestures
+    private static final int DOUBLE_TAP = 15;
+    private static final int SWIPE_UP = 2;
+    private static final int SWIPE_DOWN = 3;
+    private static final int SWIPE_RIGHT = 4;
+    private static final int SWIPE_LEFT = 5;
+    private static final int CAMERA = 6;
+
+
+    private Context mContext;
+    private SharedPreferences mPrefs;
+    private ScreenStateReceiver mScreenStateReceiver;
+    private SensorManager mSensorManager;
+    private Sensor mSensor = null;
+    private SensorEventListener mSensorEventListener;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mContext = getApplicationContext();
+        mPrefs = getSharedPreferences(ScreenOffGesture.GESTURE_SETTINGS, Activity.MODE_PRIVATE);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        mSensorEventListener = this;
+
+        Iterator iterator = mSensorManager.getSensorList(-1).iterator();
+        while (iterator.hasNext()) {
+            Sensor sensor = (Sensor) iterator.next();
+            if (sensor.getName().equals(HTC_GESTURES)) {
+                if (DEBUG) Log.d(TAG, "found gesture sensor");
+                mSensor = sensor;
+            }
+        }
+        if (mSensor != null) {
+            mSensorManager.registerListener(mSensorEventListener,
+                    mSensor, SensorManager.SENSOR_DELAY_FASTEST);
+            mScreenStateReceiver = new ScreenStateReceiver();
+            IntentFilter intentFilter = new IntentFilter();
+            intentFilter.addAction("android.intent.action.SCREEN_ON");
+            intentFilter.addAction("android.intent.action.SCREEN_OFF");
+            registerReceiver(mScreenStateReceiver, intentFilter);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mSensorManager.unregisterListener(this);
+        unregisterReceiver(mScreenStateReceiver);
+    }
+
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    public final void onAccuracyChanged(Sensor sensor, int i) {
+    }
+
+    public final void onSensorChanged(SensorEvent sensorEvent) {
+        mSensorManager.unregisterListener(mSensorEventListener);
+        float gesture = sensorEvent.values[0];
+        if (DEBUG) Log.d(TAG, "Sensor type=" + sensorEvent.sensor.getType()
+                + "," + sensorEvent.values[0] + "," + sensorEvent.values[1]);
+        startAction((int) gesture);
+    }
+
+    private void startAction(int gesture) {
+        String action = null;
+        switch (gesture) {
+            case DOUBLE_TAP:
+                action = mPrefs.getString(ScreenOffGesture.PREF_DOUBLE_TAP,
+                        ActionConstants.ACTION_WAKE_DEVICE);
+                break;
+            case SWIPE_UP:
+                action = mPrefs.getString(ScreenOffGesture.PREF_SWIPE_UP,
+                        ActionConstants.ACTION_TORCH);
+                break;
+            case SWIPE_DOWN:
+                action = mPrefs.getString(ScreenOffGesture.PREF_SWIPE_DOWN,
+                        ActionConstants.ACTION_MEDIA_PLAY_PAUSE);
+                break;
+            case SWIPE_LEFT:
+                action = mPrefs.getString(ScreenOffGesture.PREF_SWIPE_LEFT,
+                        ActionConstants.ACTION_MEDIA_PREVIOUS);
+                break;
+            case SWIPE_RIGHT:
+                action = mPrefs.getString(ScreenOffGesture.PREF_SWIPE_RIGHT,
+                        ActionConstants.ACTION_MEDIA_NEXT);
+                break;
+            case CAMERA:
+                action = mPrefs.getString(ScreenOffGesture.PREF_CAMERA,
+                        ActionConstants.ACTION_CAMERA);
+                break;
+        }
+        if (action == null || action != null && action.equals(ActionConstants.ACTION_NULL)) {
+            return;
+        }
+        if (action.equals(ActionConstants.ACTION_CAMERA)
+                || !action.startsWith("**")) {
+            Action.processAction(mContext, ActionConstants.ACTION_WAKE_DEVICE, false);
+        }
+        if (DEBUG) Log.d(TAG, action + ",gesture=" + gesture);
+        mSensorManager.registerListener(mSensorEventListener,
+                mSensor, SensorManager.SENSOR_DELAY_FASTEST);
+        Action.processAction(mContext, action, false);
+    }
+}

DeviceHandler/src/com/candy/device/util/ShortcutPickerHelper.java
@@ -1,0 +1,176 @@
+/*
+ * Copyright (C) 2011 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.candy.device.util;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
+import android.content.Intent.ShortcutIconResource;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.Log;
+
+import com.candy.device.R;
+
+import com.android.internal.util.slim.AppHelper;
+
+import java.util.ArrayList;
+
+public class ShortcutPickerHelper {
+
+    public static final int REQUEST_PICK_SHORTCUT = 100;
+    public static final int REQUEST_PICK_APPLICATION = 101;
+    public static final int REQUEST_CREATE_SHORTCUT = 102;
+
+    private Activity mParent;
+    private OnPickListener mListener;
+    private PackageManager mPackageManager;
+    private int lastFragmentId;
+
+    public interface OnPickListener {
+        void shortcutPicked(String uri, String friendlyName, Bitmap bmp, boolean isApplication);
+    }
+
+    public ShortcutPickerHelper(Activity parent, OnPickListener listener) {
+        mParent = parent;
+        mPackageManager = mParent.getPackageManager();
+        mListener = listener;
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            switch (requestCode) {
+                case REQUEST_PICK_APPLICATION:
+                    completeSetCustomApp(data);
+                    break;
+                case REQUEST_CREATE_SHORTCUT:
+                    completeSetCustomShortcut(data);
+                    break;
+                case REQUEST_PICK_SHORTCUT:
+                    processShortcut(data, REQUEST_PICK_APPLICATION, REQUEST_CREATE_SHORTCUT);
+                    break;
+            }
+        }
+    }
+
+    public void pickShortcut(int fragmentId) {
+        pickShortcut(fragmentId, false);
+    }
+
+    public void pickShortcut(int fragmentId, boolean fullAppsOnly) {
+        lastFragmentId = fragmentId;
+
+        if (fullAppsOnly) {
+            Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+            mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+            Intent pickIntent = new Intent(Intent.ACTION_PICK_ACTIVITY);
+            pickIntent.putExtra(Intent.EXTRA_INTENT, mainIntent);
+            startFragmentOrActivity(pickIntent, REQUEST_PICK_APPLICATION);
+        } else {
+            Bundle bundle = new Bundle();
+
+            ArrayList<String> shortcutNames = new ArrayList<String>();
+            shortcutNames.add(mParent.getString(R.string.group_applications));
+            bundle.putStringArrayList(Intent.EXTRA_SHORTCUT_NAME, shortcutNames);
+
+            ArrayList<ShortcutIconResource> shortcutIcons = new ArrayList<ShortcutIconResource>();
+            shortcutIcons.add(ShortcutIconResource.fromContext(mParent,
+                    android.R.drawable.sym_def_app_icon));
+            bundle.putParcelableArrayList(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, shortcutIcons);
+
+            Intent pickIntent = new Intent(Intent.ACTION_PICK_ACTIVITY);
+            pickIntent.putExtra(Intent.EXTRA_INTENT, new Intent(Intent.ACTION_CREATE_SHORTCUT));
+            pickIntent.putExtra(Intent.EXTRA_TITLE, mParent.getText(
+                    R.string.select_custom_app_title));
+            pickIntent.putExtras(bundle);
+            startFragmentOrActivity(pickIntent, REQUEST_PICK_SHORTCUT);
+        }
+    }
+
+    private void startFragmentOrActivity(Intent pickIntent, int requestCode) {
+        if (lastFragmentId == 0) {
+            mParent.startActivityForResult(pickIntent, requestCode);
+        } else {
+            Fragment cFrag = mParent.getFragmentManager().findFragmentById(lastFragmentId);
+            if (cFrag != null) {
+                mParent.startActivityFromFragment(cFrag, pickIntent, requestCode);
+            }
+        }
+    }
+
+    private void processShortcut(Intent intent,
+        int requestCodeApplication, int requestCodeShortcut) {
+        // Handle case where user selected "Applications"
+        String applicationName = mParent.getResources().getString(R.string.group_applications);
+        String shortcutName = intent.getStringExtra(Intent.EXTRA_SHORTCUT_NAME);
+
+        if (applicationName != null && applicationName.equals(shortcutName)) {
+            Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+            mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+            Intent pickIntent = new Intent(Intent.ACTION_PICK_ACTIVITY);
+            pickIntent.putExtra(Intent.EXTRA_INTENT, mainIntent);
+            startFragmentOrActivity(pickIntent, requestCodeApplication);
+        } else {
+            startFragmentOrActivity(intent, requestCodeShortcut);
+        }
+    }
+
+    private void completeSetCustomApp(Intent data) {
+        mListener.shortcutPicked(data.toUri(0),
+            AppHelper.getFriendlyActivityName(mParent, mPackageManager, data, false), null, true);
+    }
+
+    private void completeSetCustomShortcut(Intent data) {
+        Intent intent = data.getParcelableExtra(Intent.EXTRA_SHORTCUT_INTENT);
+        /* preserve shortcut name, we want to restore it later */
+        intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, data.getStringExtra(
+                Intent.EXTRA_SHORTCUT_NAME));
+        String appUri = intent.toUri(0);
+        appUri = appUri.replaceAll("com.android.contacts.action.QUICK_CONTACT", Intent.ACTION_VIEW);
+
+        // Check if icon is present
+        Bitmap bmp = null;
+        Parcelable extra = data.getParcelableExtra(Intent.EXTRA_SHORTCUT_ICON);
+        if (extra != null && extra instanceof Bitmap) {
+            bmp = (Bitmap) extra;
+        }
+        // No icon till now check if icon resource is present
+        if (bmp == null) {
+            extra = data.getParcelableExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE);
+            if (extra != null && extra instanceof Intent.ShortcutIconResource) {
+                try {
+                    Intent.ShortcutIconResource iconResource = (ShortcutIconResource) extra;
+                    Resources resources =
+                            mPackageManager.getResourcesForApplication(iconResource.packageName);
+                    final int id = resources.getIdentifier(iconResource.resourceName, null, null);
+                    bmp = BitmapFactory.decodeResource(resources, id);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        mListener.shortcutPicked(appUri,
+                AppHelper.getFriendlyShortcutName(mParent, mPackageManager, intent), bmp, false);
+    }
+
+}